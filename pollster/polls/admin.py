1781ec0cb455ca43e9a0d7582b56618cccfac45f
alatos2 <alabitosin60@gmail.com>
2020-05-17 18:51:28 +0100

    first commit

---
+++ pollster/polls/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from .models import Question, Choice
+
+# Make the poll app modifiable in the admin
+
+# admin.site.register(Question)
+# admin.site.register(Choice)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, { 'fields': ['question_text'] }),
+        ('Date Information', { 'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
+
+admin.site.site_header = "Pollster Admin"
+admin.site.site_title = "Pollster Admin Area"
+admin.site.index_title = "Welcome to the Pollster Admin Area"
